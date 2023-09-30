7ecc98e2c23662780f408a37782ee0bc218a2b00
Srikanth <srikanthrdy7979@gmail.com>
2023-10-01 00:06:08 +0530

    new branch

--- src/main/java/com/login/MongoDBManager.java
+++ src/main/java/com/login/MongoDBManager.java
@@ -1,13 +1,14 @@
 package com.login;
-
 import com.mongodb.MongoClientSettings;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoDatabase;
+import com.mongodb.ServerAddress;
+import java.util.Arrays;
 
 public class MongoDBManager {
     private static final String DATABASE_NAME = "mydb"; // Change to your desired database name
-    private static final String MONGO_HOST = "my-mongodb"; // Use the Docker service name
+    private static final String MONGO_HOST = "mongodb"; // Use the Docker service name
 
     public static MongoClient connect() {
         MongoClientSettings settings = MongoClientSettings.builder()
